cdbe94453ff0d2772210f46e714ccaf668a8a727
Chirag Garg <chiraggarg2662002@gmail.com>
2022-01-21 10:41:52 +0530

    auth done

--- app.js
+++ app.js
@@ -1,111 +1,71 @@
-const express = require('express');
-const ejs = require('ejs');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session')
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
 const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+require('./auth');
+
 const app = express();
 
-app.set("view engine","ejs")
-app.use(bodyParser.urlencoded({ extended: true}));
-
-mongoose.connect('mongodb://localhost:27017/StudentDB');
-
-app.use(session({
-  secret: 'Gate pass for all',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
 }));
-// app. id is false and if we got the jjnl
+
+
+
+function isLoggedIn(req, res, next) {
+  req.user ? next() : res.sendStatus(401);
+}
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-const UserSchema = new mongoose.Schema({
-  name:String,
-  password:String
-});
-UserSchema.plugin(passportLocalMongoose);
-
-const Student= new mongoose.model("Student",UserSchema);
-passport.use(Student.createStrategy());
-
-passport.serializeUser(Student.serializeUser());
-passport.deserializeUser(Student.deserializeUser());
-
-
-app.get("/",(req,res)=>{
-  // var newDate = Date();
-  // res.render("index" ,{date:newDate});
+app.get('/', (req, res) => {
   res.render("main")
 });
-app.get("/index",(req,res)=>{
-  console.log(req.isAuthenticated());
-  //
-  // if(){
-  //   res.render("index");
-  // }else{
-  //   res.send("hii")
-  // }
 
-});
-app.get("/login",(req,res)=>{
-  res.render("login")
-});
-app.get("/register",(req,res)=>{
-  res.render("register")
-});
+app.get('/auth/google',
+  passport.authenticate('google', { scope: [ 'email', 'profile' ] }
+));
 
-app.post("/register", function(req, res){
-
-  User.register({username: req.body.email}, req.body.password, function(err, user){
-    if (err) {
-      console.log(err);
-      res.redirect("/register");
-    } else {
-      passport.authenticate("local")(req, res, function(){
-        res.redirect("/index");
-      });
-    }
-  });
-
-});
-
-app.post("/login",(req,res)=>{
-  const student = new Student({
-    username:req.body.email,
-    password:req.body.password
-  });
-  req.login(student,function(err){
-    if(err){
-      console.log(err);
-    }else {
-      passport.authenticate("local")(req, res, function(){
-        console.log("hiii");
-        res.redirect("/index");
-      });
-    }
+app.get( '/login/google',
+  passport.authenticate( 'google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/google/failure'
   })
+);
+
+app.get('/protected', isLoggedIn, (req, res) => {
+  mail= req.user.email;
+  mail = mail.substr(-13, 13);
+  if(mail=="@iitbbs.ac.in")
+  {
+    var date = new Date();
+   date = date.getDate();
+  res.render("index",{user:req.user,date:date});}else{
+    res.send("Please log in with IIT Bhubaeswar ID")
+  }
+  
+
+
 });
 
-app.post("/gaurd",(req,res)=>{
-  const details = {
-    roll :req.body.RollNumber,
-    name :req.body.Name,
-    date:req.body.date
+app.post('/submit', (req, res) => {
+  req.logout();
+  req.session.destroy();
+  res.send('Goodbye!');
+});
 
-  };
-res.render("gaurd",{details:details})
-
+app.get('/auth/google/failure', (req, res) => {
+  res.send('Failed to authenticate..');
 });
 
 
 
-
-
-
-app.listen(3000,(err)=>{
-  console.log("Serer is running at port 3000");
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
 });
